f6ec89ba7747c4ec7fa17d18336a2e057072166f
Adam-D-Beno <wrofdt@yandex.ru>
2022-06-21 13:39:37 +0300

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,4 +1,4 @@
-package ru.job4j;
+package ru.job4j.calculator;
 
 public class Calculator {
     public static void main(String[] args) {
@@ -20,3 +20,4 @@ public class Calculator {
         System.out.println(fourTimeTwo);
     }
 }
+
